ba23427d57386762896d594ccc8efbe1d71f1b8e
TVH Client <noreply@local>
2026-06-18 17:36:59 +0200

    M114-fix: versionCode zvyseny na 4 (3 uz bol pouzity v Play)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "sk.tvhclient"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
     }
 
